00e20a5db2f9b5c667699ad7a217cde80b9a6445
Milou Sanden <Milou.Sanden@gmail.com>
2024-12-03 15:09:52 +0100

    sets on sakila

--- exercises/exercise2/3/sql/set_sakila.sql
+++ exercises/exercise2/3/sql/set_sakila.sql
@@ -1,5 +1,21 @@
 -- a) retrieve a list of all customers and actors wich last name start with G.
+SELECT * FROM main.customer c;
+
+SELECT
+	c.last_name
+FROM
+	main.customer c
+WHERE
+	c.last_name LIKE 'G%'
+UNION 
+SELECT 
+	a.last_name
+FROM
+	main.actor a
+WHERE
+	a.last_name LIKE 'G%';
 
 -- b) how many customers and actors starts with 'ann' in their first names?
+SELECT COUNT(c.first_name) FROM main.customer c;
 
 -- c) in which cities and countries do the costumers live in?
