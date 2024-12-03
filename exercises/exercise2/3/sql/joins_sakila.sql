583d45280b1eddc76a9673bc9682335dc083e13f
Milou Sanden <Milou.Sanden@gmail.com>
2024-12-03 14:49:55 +0100

    setting up sets and joins sqls for sakila

---
+++ exercises/exercise2/3/sql/joins_sakila.sql
@@ -0,0 +1,3 @@
+-- d) in which cities and countris do the customers with initials JD live in?
+
+-- e) retreive a list of all customers and what films they have rented.
---
+++ exercises/exercise2/3/sql/set_sakila.sql
@@ -0,0 +1,5 @@
+-- a) retrieve a list of all customers and actors wich last name start with G.
+
+-- b) how many customers and actors starts with 'ann' in their first names?
+
+-- c) in which cities and countries do the costumers live in?
